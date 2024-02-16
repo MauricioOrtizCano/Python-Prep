2c8dd8d687933c6ef240d464fe0d48e726d64aee
MauricioOrtizCano <cmoc14@yahoo.com>
2024-02-15 23:14:09 -0500

    Changes in Calculadora.py

--- M08_clasesyOOP/Calculadora.py
+++ M08_clasesyOOP/Calculadora.py
@@ -1,19 +1,34 @@
 class CalculadoraM08:
     def __init__(self, lista):
-        self.lista = lista
+        if (type(lista) != list):
+            self.lista = []
+            raise ValueError('Debe ser pasado como parametro una lista')
+        else:
+            self.lista = lista
+        
 
-    def es_primo(self, num):
+    def es_primo(self):
         '''
         Determina si un numero es primo o no
         '''
-        if (num == 0 or num == 1 or num == 4):
-            return False
-    
-        for i in range(2,num):
-            if(num % i == 0):
+        def demostrar(numero):
+            if (numero == 0 or numero == 1 or numero == 4):
                 return False
         
-        return True
+            for i in range(2,numero):
+                if(numero % i == 0):
+                    return False
+            
+            return True
+        
+        primos = []
+
+        for i in self.lista:
+            primos.append(demostrar(i))
+
+        return primos
+
+
     
     def repetido(self):
         '''
@@ -49,6 +64,7 @@ class CalculadoraM08:
         return mayor_llave, mayor_valor
     
     def conversion_temperatura (self, valor, me_origen, me_destino):
+        
         if(me_origen == "°C"):
             if(me_destino == "°F"):
                 to_far = (valor * 9/5) + 32
@@ -63,13 +79,16 @@ class CalculadoraM08:
             else:
                 to_kelv = (valor - 32) * 5/9 + 273.15
                 return f'{to_kelv} °K'
-        else:
+        elif(me_origen == "°K"):
             if(me_destino == "°C"):
                 to_cel = valor - 273.15
                 return f'{to_cel} °C'
             else:
                 to_fare = (valor - 273.15) * 9/5 + 32
                 return f'{to_fare} °F'
+        else:
+            raise ValueError("Las medidas de origen o de destino deben ser °C, °K, °F")
+
             
     def factorial(self, num):
         if(num < 0 or type(num) != int):
@@ -81,3 +100,10 @@ class CalculadoraM08:
         num = num * self.factorial(num-1)
 
         return num
+    
+
+
+
+imprimir = CalculadoraM08([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
+
+print(imprimir.es_primo())
