9cb834a46265d411a0f9afaba4ea8125253cb979
MauricioOrtizCano <cmoc14@yahoo.com>
2024-02-17 19:36:56 -0500

    10th almost done

--- M10_manejodearchivos/clase09_ej1.py
+++ M10_manejodearchivos/clase09_ej1.py
@@ -1,5 +1,9 @@
 import sys
 # Comprobación de seguridad, ejecutar sólo si se recibe 3 argumentos
+for i in range(1,4):
+    variable = input('Escribe algo: ')
+    sys.argv.append(variable)
+
 if len(sys.argv) == 4:
     print("El primer parámetro es:",sys.argv[1])
     print("El segundo parámetro es:",sys.argv[2])
@@ -7,3 +11,6 @@ if len(sys.argv) == 4:
 else:
     print("ERROR: Introdujo una cantidad de argumentos distinta de tres (3)")
     print('Ejemplo: clase09_ej1.py 1 2 3')
+
+
+print(sys.argv)
